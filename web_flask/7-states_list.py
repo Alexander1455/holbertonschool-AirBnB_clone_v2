90065745085d1c17eb1a19a8be9c7fffd433bea6
Alexander1455 <alexander21111121@gmail.com>
2023-09-17 23:24:58 -0500

    code restructuring

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""  script that starts a Flask web application """
+"""Module to start a Flask web application"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    return render_template("7-states_list.html", storages=storage.all(State))
+
+
 @app.teardown_appcontext
-def close(exception):
-    """ Close session"""
+def remove_current_sql_session(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ Show in HTML page with a list of all State """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
-
-
-if __name__ == '__main__':
-    """ start the web app """
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
